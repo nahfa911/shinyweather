47e8885eba0bb7369c2124e722a56d7d87fa358c
nahfa911 <nahidfarazmand@gmail.com>
2018-09-30 20:54:59 +0200

    update ui

--- inst/myapp/ui.R
+++ inst/myapp/ui.R
@@ -1,5 +1,5 @@
 shiny::fluidPage(
-  shiny::titlePanel(div("Weather forecat",style="background-color:aliceblue;text-align:center;
+  shiny::titlePanel(div("Weather forecast",style="background-color:aliceblue;text-align:center;
                         font-family:Calibri;height:35px;border-radius:6px;"
   )
   )
